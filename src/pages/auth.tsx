d83c72c66e3667464ea53912ed629f472f924235
geiiwe <diariaironia@gmail.com>
2025-06-09 12:46:04 -0300

    pagina de cadastro

--- src/pages/auth.tsx
+++
@@ -1,5 +0,0 @@
-<Tabs defaultValue="signup" className="w-full">
-  <TabsList className="grid w-full grid-cols-1 mb-6">
-    <TabsTrigger value="signup">Cadastrar</TabsTrigger>
-  </TabsList>
-</Tabs> 
