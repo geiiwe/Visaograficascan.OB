e4ca02a7e1c8dda98fe165d6ac025f8543af7c5f
geiiwe <diariaironia@gmail.com>
2025-06-09 12:28:49 -0300

    pagina de cadastro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import { Seo } from '@/components/Seo';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Index = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/auth', { replace: true });
+  }, [navigate]);
+
   return (
     <>
       <Seo title="Analisador Gráfico Inteligente" description="Ferramenta inteligente de análise gráfica com visão computacional." />
